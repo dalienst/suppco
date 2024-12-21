951675f4f0a379b5414f63276c1a4405035f2152
dalienst <oduordalienst@gmail.com>
2024-12-21 09:54:49 +0300

    fixed branch employees

--- actionForms/branches/AddEmployeeToBranch.jsx
+++ actionForms/branches/AddEmployeeToBranch.jsx
@@ -1,11 +1,17 @@
 import { Button } from "@/app/components/ui/button";
 import useAxiosAuth from "@/hooks/useAxiosAuth";
-import { updateSite } from "@/services/sites";
+import { updateBranch } from "@/services/branches";
 import { Field, Form, Formik } from "formik";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 
-function AddEmployeeToBranch({ site, branch, profile, slug, refetchSite, closeEmployeeSelectionPanel }) {
+function AddEmployeeToBranch({
+  branch,
+  profile,
+  slug,
+  refetchBranch,
+  closeEmployeeSelectionPanel,
+}) {
   const [loading, setLoading] = useState(false);
   const axios = useAxiosAuth();
 
@@ -22,9 +28,9 @@ function AddEmployeeToBranch({ site, branch, profile, slug, refetchSite, closeEm
           values.employees.forEach((employee) =>
             formData.append("employees", employee)
           );
-          await updateSite(slug, formData, axios);
+          await updateBranch(formData, axios, slug);
           toast.success("Employee(s) added successfully. Refreshing...");
-          refetchSite();
+          refetchBranch();
         } catch (error) {
           toast.error("Failed to add employee(s)");
         } finally {
@@ -34,71 +40,66 @@ function AddEmployeeToBranch({ site, branch, profile, slug, refetchSite, closeEm
     >
       {({ values, setFieldValue }) => (
         <Form>
-              <div>
-                <div className="flex justify-between items-center">
-                <span className="text-lg underline">Select employee(s) :</span>
-                  <div className="flex items-center">
-                    <input
-                      type="checkbox"
-                      className="mr-2"
-                      onChange={(e) => {
-                        const isChecked = e.target.checked;
-                        const allWorkers =
-                          profile?.companies?.company_employees?.map(
-                            (worker) => worker.email
-                          );
-                        setFieldValue("employees", isChecked ? allWorkers : []);
-                      }}
-                      checked={
-                        values.employees.length ===
-                        profile?.companies?.company_employees?.length
-                      }
-                    />
-                    <span className="text-sm">Select All</span>
-                  </div>
-                </div>
-                <ul className="max-h-[300px] overflow-auto">
-                  {profile?.companies?.company_employees?.map((worker) => (
-                    <li key={worker?.id} className="flex items-center py-2">
-                      <input
-                        type="checkbox"
-                        id={worker?.id}
-                        className="mr-2"
-                        value={worker?.email}
-                        checked={values.employees.includes(worker?.email)}
-                        onChange={(e) => {
-                          const isChecked = e.target.checked;
-                          const updatedEmployees = isChecked
-                            ? [...values.employees, worker.email]
-                            : values.employees.filter(
-                                (email) => email !== worker.email
-                              );
-                          setFieldValue("employees", updatedEmployees);
-                        }}
-                      />
-                      <label
-                        htmlFor={worker?.id}
-                        className="text-sm"
-                      >
-                        {worker?.email}
-                      </label>
-                    </li>
-                  ))}
-                </ul>
+          <div>
+            <div className="flex justify-between items-center">
+              <span className="text-lg underline">Select employee(s) :</span>
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  className="mr-2"
+                  onChange={(e) => {
+                    const isChecked = e.target.checked;
+                    const allWorkers =
+                      profile?.companies?.company_employees?.map(
+                        (worker) => worker.email
+                      );
+                    setFieldValue("employees", isChecked ? allWorkers : []);
+                  }}
+                  checked={
+                    values.employees.length ===
+                    profile?.companies?.company_employees?.length
+                  }
+                />
+                <span className="text-sm">Select All</span>
               </div>
+            </div>
+            <ul className="max-h-[300px] overflow-auto">
+              {profile?.companies?.company_employees?.map((worker) => (
+                <li key={worker?.id} className="flex items-center py-2">
+                  <input
+                    type="checkbox"
+                    id={worker?.id}
+                    className="mr-2"
+                    value={worker?.email}
+                    checked={values.employees.includes(worker?.email)}
+                    onChange={(e) => {
+                      const isChecked = e.target.checked;
+                      const updatedEmployees = isChecked
+                        ? [...values.employees, worker.email]
+                        : values.employees.filter(
+                            (email) => email !== worker.email
+                          );
+                      setFieldValue("employees", updatedEmployees);
+                    }}
+                  />
+                  <label htmlFor={worker?.id} className="text-sm">
+                    {worker?.email}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className="w-[300px] mt-3 flex justify-between gap-2">
-                          <Button disabled={loading}>
-                            Add employee(s)
-                          </Button>
-                          <Button
-                            variant="outline"
-                            disabled={loading}
-                            type='button'
-                            onClick={closeEmployeeSelectionPanel}
-                          >
-                            Cancel
-                          </Button>
-                        </div>
+            <Button disabled={loading}>Add employee(s)</Button>
+            <Button
+              variant="outline"
+              disabled={loading}
+              type="button"
+              onClick={closeEmployeeSelectionPanel}
+            >
+              Cancel
+            </Button>
+          </div>
         </Form>
       )}
     </Formik>
--- app/(routes)/branch/[slug]/employees/page.jsx
+++ app/(routes)/branch/[slug]/employees/page.jsx
@@ -1,10 +1,17 @@
-'use client'
+"use client";
 
 import SupplierLoadingSpinner from "@/components/supplier/LoadingSpinner";
 import UserTable from "@/components/tables/InfoTable";
 import { employeeColumn } from "@/data/columns";
 import { useFetchBranchDetail } from "@/dataActions/branches/branchesActions";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/app/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/components/ui/dialog";
 import { useParams } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/app/components/ui/button";
@@ -20,59 +27,64 @@ function BranchEmployees() {
   const [selectedEmployeeIds, setSelectedEmployeeIds] = useState([]);
   const [openEmployeeSelectionPanel, setOpenEmployeeSelectionPanel] =
     useState(false);
-  const {slug} = useParams()
+  const { slug } = useParams();
   const {
     isLoading: isLoadingBranch,
     data: branch,
     refetch: refetchBranch,
-    isSuccess
+    isSuccess,
   } = useFetchBranchDetail(slug);
+
   const axios = useAxiosAuth();
   const userId = useUserId();
   const { isLoading: isLoadingUser, data: profile } = useQuery({
     queryKey: ["profile"],
     queryFn: () => getUser(userId, axios),
   });
-  console.log(profile,'profile')
-  console.log(branch,'branch')
+  console.log(profile, "profile");
+  console.log(branch, "branch");
 
   return (
     <div>
       <div className="border-b py-3 mb-5 flex items-center justify-between gap-5">
-       <h2 className="text-lg font-semibold">{branch?.name}&apos;s Employees</h2>
-       <div className="relative">
-              <button
-                onClick={() => setOpenEmployeeSelectionPanel((prev) => !prev)}
-                className="border border-grayBlue rounded-lg p-2 flex gap-2 items-center"
-              >
-                <span>Add employee(s) to this branch</span>
-                <ChevronDown size={16} />
-              </button>
-             
-            {openEmployeeSelectionPanel && (
-              <div className="absolute bg-white z-50 mt-2 border shadow rounded-lg right-0 p-2 w-[320px]">
-                <AddEmployeeToBranch
-              site={branch}
-              // profile={profile}
-              slug={slug}
-              refetchSite={refetchSite}
-              closeEmployeeSelectionPanel={()=>setOpenEmployeeSelectionPanel(false)}
-            />
-              </div>
-            )}
-          </div>
-      </div>
-          {isLoadingBranch ? (
-            <SupplierLoadingSpinner />
-          ) : branch?.branch_employees?.length > 0 ? (
-            <UserTable rows={branch?.branch_employees} columns={employeeColumn} />
-          ) : (
-            <div className="place-content-center text-center">
-              <h6>You have no Employees</h6>
+        <h2 className="text-lg font-semibold">
+          {branch?.name}&apos;s Employees
+        </h2>
+        <div className="relative">
+          <button
+            onClick={() => setOpenEmployeeSelectionPanel((prev) => !prev)}
+            className="border border-grayBlue rounded-lg p-2 flex gap-2 items-center"
+          >
+            <span>Add employee(s) to this branch</span>
+            <ChevronDown size={16} />
+          </button>
+
+          {openEmployeeSelectionPanel && (
+            <div className="absolute bg-white z-50 mt-2 border shadow rounded-lg right-0 p-2 w-[320px]">
+              <AddEmployeeToBranch
+                branch={branch}
+                profile={profile}
+                slug={slug}
+                refetchBranch={refetchBranch}
+                closeEmployeeSelectionPanel={() =>
+                  setOpenEmployeeSelectionPanel(false)
+                }
+              />
             </div>
           )}
         </div>
-  )
+      </div>
+      {/* {isLoadingBranch ? (
+        <SupplierLoadingSpinner />
+      ) : branch?.employees?.length > 0 ? (
+        <UserTable rows={branch?.employees} columns={employeeColumn} />
+      ) : (
+        <div className="place-content-center text-center">
+          <h6>You have no Employees</h6>
+        </div>
+      )} */}
+    </div>
+  );
 }
 
-export default BranchEmployees
+export default BranchEmployees;
