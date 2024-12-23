704e44fb897d6f66071346508f31b884386daec8
dalienst <oduordalienst@gmail.com>
2024-12-23 17:00:21 +0300

    added walls function

--- app/(routes)/branch/[slug]/shell/[categorySlug]/[itemSlug]/page.jsx
+++ app/(routes)/branch/[slug]/shell/[categorySlug]/[itemSlug]/page.jsx
@@ -69,7 +69,14 @@ export default function ItemDetail({
       case "detail-subcomponentsaccessories":
         return <AddAccessories />;
       case "walls":
-        return <AddWalls />;
+        return (
+          <AddWalls
+            item={subCategoryItem}
+            category={category}
+            refetchShell={refetchShell}
+            employees={branch?.employees}
+          />
+        );
       case "roof":
         return <AddRoof />;
       case "pre-cast":
