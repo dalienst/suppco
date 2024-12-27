67fab5a08908d23eba58b3ce11f46097e72a3704
Tony Ligogo <tonyligogo@gmail.com>
2024-12-28 01:41:09 +0300

    shell equipment details

--- app/(routes)/branch/[slug]/product-detail/[id]/page.jsx
+++ app/(routes)/branch/[slug]/product-detail/[id]/page.jsx
@@ -7,8 +7,8 @@ function ProductDetail() {
     const {isLoading , data} = useFetchShellEquipmentDetail()
     console.log(data,'data from shell equipment detail')
   return (
-    <div className="pt-4">
-        <div className='flex justify-between items-center gap-5'>
+    <div className="pt-4 overflow-hidden">
+        <div className='flex justify-between items-center gap-5 mb-3'>
             <span className="font-semibold text-xl">Product name</span>
             <div>
                 <Button variant='outline' className='text-blue-600 h-8 mr-2 border-blue-600'>Edit</Button>
@@ -16,10 +16,10 @@ function ProductDetail() {
             </div>
         </div>
         <hr />
-        <span className="font-semibold text-xl">Product Details</span>
+        <span className="font-semibold text-xl block py-2">Product Details</span>
         <hr />
-        <div className="w-full overflow-hidden">
-        <table className="border overflow-auto">
+        <div className="w-full overflow-x-auto mt-2 pb-4">
+        <table className="border">
             <thead>
                 <tr>
                     <td className="border px-2">Name</td>
@@ -37,6 +37,61 @@ function ProductDetail() {
                 </tr>
             </tbody>
         </table>
+        </div>
+        <hr />
+        <span className="font-semibold text-xl block py-2">Delivery Information</span>
+        <hr />
+        <div className="mt-2 border rounded-lg overflow-hidden">
+            <span className="bg-blue-800 px-3 py-1 text-white block w-full">Delivery Details</span>
+            <div className="p-3">
+            <span className="block">
+                <span className="font-semibold">Availability:</span>
+                <span>Yes</span>
+            </span>
+            <span className="block">
+                <span className="font-semibold">Pricing criteria:</span>
+                <span>Distance(km)</span>
+            </span>
+            <span className="">
+                <span className="font-semibold">Cost:</span>
+                <span>1000</span>
+            </span>
+            </div>
+        </div>
+        <hr />
+        <span className="font-semibold text-xl block py-2">Payment Information</span>
+        <hr />
+        <div className="mt-2 border rounded-lg overflow-hidden">
+            <span className="bg-blue-800 px-3 py-1 text-white block w-full">Payment Details</span>
+            <div className="p-3">
+            <span className="block">
+                <span className="font-semibold">Quantity:</span>
+                <span>500 bags</span>
+            </span>
+            <span className="block">
+                <span className="font-semibold">Price per bag:</span>
+                <span>Ksh 800</span>
+            </span>
+            <span className='font-semibold'>Payment plan:</span>
+            <div className="w-full overflow-x-auto mt-2 pb-4">
+        <table className="border">
+            <thead>
+                <tr>
+                    <td className="border px-2">Plan</td>
+                    <td className="border px-2">Condition</td>
+                    <td className="border px-2">Deposit</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className="border px-2">Fixed upfront payment</td>
+                    <td className="border px-2">Upfront payment</td>
+                    <td className="border px-2">0</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+            </div>
         </div>
     </div>
   )
