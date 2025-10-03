0a0df4b4111ffcbba50cf8d1d482ecf342894ca8
Dalienst Owino <oduordalienst@gmail.com>
2025-10-03 12:07:02 +0300

    modify url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,7 +19,7 @@ export const authOptions = {
 
         // TODO: switch to axios
         const response = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/accounts/token/`,
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/login/`,
           {
             method: "POST",
             headers: {
